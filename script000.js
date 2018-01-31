cdcc744fdd5563a64c83ef389759645894bca78a
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2018-01-31 11:37:27 +0100

    SMedia bijgewerkt en Hosting pakketten afgerond

--- script000.js
+++ script000.js
@@ -1,6 +1,18 @@
 function Brons(){
-	var Brons = "Pakket Brons: 10GB HDD, 512MB RAM, 1 Core, 100GB dataverkeer per maand"
+	var Brons = "Pakket Brons: 10GB HDD, 512MB RAM, 1 Core, 100GB dataverkeer,"
 	var PBrons = "€5,95 per maand"
 
-	document.getElementById('Brons').innerHTML = Brons + " " + PBrons;
+	document.getElementById('Out').innerHTML = Brons + " " + PBrons;
+}
+function Zilver(){
+	var Zilver = "Pakket Zilver: 20GB HDD, 1048MB RAM, 2 Cores, 200GB dataverkeer,"
+	var PZilver = "€7,95 per maand"
+
+	document.getElementById('Out').innerHTML = Zilver + " " + PZilver;
+}
+function Goud(){
+	var Goud = "Pakket Goud: 50GB HDD, 2048MB RAM, 2 Cores, 200GB dataverkeer,"
+	var PGoud = "€10,95 per maand"
+
+	document.getElementById('Out').innerHTML = Goud + " " + PGoud;
 }
