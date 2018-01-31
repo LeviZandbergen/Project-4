879159d747146375feacef6ad876c259ca00af87
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2018-01-31 09:56:09 +0100

    Hosting pakketten met javascript functie

---
+++ script000.js
@@ -0,0 +1,6 @@
+function Brons(){
+	var Brons = "Pakket Brons: 10GB HDD, 512MB RAM, 1 Core, 100GB dataverkeer per maand"
+	var PBrons = "€5,95 per maand"
+
+	document.getElementById('Brons').innerHTML = Brons + " " + PBrons;
+}
