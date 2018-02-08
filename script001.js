a8d23a733c2209ce0620f3e23121617cb731d239
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2018-02-08 10:02:07 +0100

    hele site goed

--- script001.js
+++ script001.js
@@ -14,10 +14,70 @@ function carousel() {
 }
 
 function Rekentool() {
-	var Pbrons = "5,95";
-	var Pzilver = "7,95";
-	var pgoud = "10.95"
-	var vraag1 = "Om welk pakket gaat het?"
+	var Pbrons = 5.95;
+	var Pzilver = 7.95;
+	var Pgoud = 10.95;
+	var PgoudJ = Pgoud * 12;
+	var PgoudF = Math.round(PgoudJ).toFixed(0);
+	var vraag1 = "Om welk pakket gaat het?";
+	var vraag2 = "Om hoe lang gaat het?"
 
-	prompt(vraag1)
+	var AV1 = prompt(vraag1);
+
+	if (AV1.toUpperCase() === "BRONS") {
+		var AV2 = prompt(vraag2, "1 Maand / 1 Jaar / 5 Jaar")
+		berekenWaardes(AV2, Pbrons);
+
+			// if (AV2.toUpperCase() === "1 Maand".toUpperCase()){
+			// 	alert("1 Maand kost" + " " + "€" + Pbrons)
+			// 	}
+			// 	else if (AV2.toUpperCase() === "1 Jaar".toUpperCase()){
+			// 		alert("1 Jaar kost:" + " " + "€" + Pbrons * 12);
+			// 	}
+				
+			// 	else if (AV2.toUpperCase() === "5 Jaar".toUpperCase()){
+			// 		alert("5 Jaar kost:" + " " + "€" + Pbrons * 60);
+			// 	}
+	}
+	else if (AV1.toUpperCase() === "zilver".toUpperCase()) {
+		var AV2 = prompt(vraag2, "1 Maand / 1 Jaar / 5 Jaar")
+		berekenWaardes(AV2, Pzilver);
+			// if (AV2.toUpperCase() === "1 Maand".toUpperCase()){
+			// 	alert("1 Maand kost" + " " + "€" + Pzilver)
+			// 	}
+			// 	else if (AV2.toUpperCase() === "1 Jaar".toUpperCase()){
+			// 		alert("1 Jaar kost:" + " " + "€" + Pzilver * 12);
+			// 	}
+				
+			// 	else if (AV2.toUpperCase() === "5 Jaar".toUpperCase()){
+			// 		alert("5 Jaar kost:" + " " + "€" + Pzilver * 60);
+			// 	}
+	}
+	else if (AV1.toUpperCase() === "goud".toUpperCase()) {
+		var AV2 = prompt(vraag2, "1 Maand / 1 Jaar / 5 Jaar")
+		berekenWaardes(AV2, Pgoud);
+
+			// if (AV2.toUpperCase() === "1 Maand".toUpperCase()){
+			// 	alert("1 Maand kost" + " " + "€" + Pgoud)
+			// 	}
+			// 	else if (AV2.toUpperCase() === "1 Jaar".toUpperCase()){
+			// 		alert("1 Jaar kost:" + " " + "€" + PgoudF);
+			// 	}
+				
+			// 	else if (AV2.toUpperCase() === "5 Jaar".toUpperCase()){
+			// 		alert("5 Jaar kost:" + " " + "€" + Pgoud * 60);
+			//	}
+	}
+
+	function berekenWaardes(AV2, pakketWaarde) {
+		if (AV2.toUpperCase() === "1 Maand".toUpperCase()){
+			alert("1 Maand kost" + " " + "€" + pakketWaarde)
+		}
+		else if (AV2.toUpperCase() === "1 Jaar".toUpperCase()){
+			alert("1 Jaar kost:" + " " + "€" + pakketWaarde);
+		}
+		else if (AV2.toUpperCase() === "5 Jaar".toUpperCase()){
+			alert("5 Jaar kost:" + " " + "€" + pakketWaarde * 60);
+		}
+	}
 }
